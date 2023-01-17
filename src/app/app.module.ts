86e812f65b5853b56b51b1aebd547cd1e079008e
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-01-17 22:32:25 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http"
 import { CoinComponent } from './components/coin/coin.component'
 
 @NgModule({
