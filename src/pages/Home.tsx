d3617b6a4fa2a6dcef02acc79562f5c93f1c5270
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-03-15 22:43:01 +0530

    added menu bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import MenuAppBar from '../components/MenuAppBar'
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <MenuAppBar />
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
