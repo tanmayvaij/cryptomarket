5bdafd14ac683fc1a72fdc3a3da370b6c9dbf700
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-03-16 21:59:27 +0530

    project reset

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
-import MenuAppBar from '../components/MenuAppBar'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <MenuAppBar />
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
     </IonPage>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
